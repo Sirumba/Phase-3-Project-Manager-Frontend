5b030f9ca4962ce7a8e4833961bf4a5fd0f49285
Sirumba <simonsmaina45@gmail.com>
2023-04-12 21:21:06 +0300

    home

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from "react";
+import HomeCarousel from "./HomeCarousel";
+
+export default function Home(){
+    return(
+        <div style={{background:"#242842"}}>
+            <HomeCarousel/>
+        </div>
+    )
+}
